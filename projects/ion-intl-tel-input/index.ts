5b5db6518a8e7e297c2570811506650a80f1d161
Doug Kneller <dkneller@gmail.com>
2025-01-14 20:25:14 -0700

    update scripts to move tgz file to accessible location

---
+++ projects/ion-intl-tel-input/index.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of ion-intl-tel-input
+ */
+
+export * from './src/lib/ion-intl-tel-input.directive';
+export * from './src/lib/ion-intl-tel-input/ion-intl-tel-input.component';
+export * from './src/lib/ion-intl-tel-input/ion-intl-tel-code.component';
+export * from './src/lib/ion-intl-tel-input.service';
+export * from './src/lib/ion-intl-tel-input.module';
